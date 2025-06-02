7e3b92ef17e76b8d165a56daa1cc30aa3e5a0cc6
clothingquality <clothingquality69@gmail.com>
2025-06-02 13:43:46 -0500

    Agregando ip de user

--- src/main/java/com/co/quality/clothing/utils/Constants.java
+++ src/main/java/com/co/quality/clothing/utils/Constants.java
@@ -3,5 +3,5 @@ package com.co.quality.clothing.utils;
 public class Constants {
     //Cross Origins Constants
     public static final String ADMIN_PAGE = "http://127.0.0.1:5502/";
-    public static final String USERS_PAGE = "http://localhost:4200/";
+    public static final String USERS_PAGE = "http://127.0.0.1:5500/";
 }
