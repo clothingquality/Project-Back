fbc6f184d526aedad2bd4c2d0ac742b4ed0383dd
clothingquality <clothingquality69@gmail.com>
2025-07-25 13:03:17 -0500

    Cambio faltante corss

--- src/main/java/com/co/quality/clothing/config/CorsConfig.java
+++ src/main/java/com/co/quality/clothing/config/CorsConfig.java
@@ -1,20 +1,21 @@
 package com.co.quality.clothing.config;
 
 import com.co.quality.clothing.utils.Constants;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://qualityclothingcol.com",
-                        "https://testsitedomainparapagina.io",
-                        "http://127.0.0.1:5500",
-                        "http://127.0.0.1:5502"
+                        Constants.ADMIN_PAGE,
+                        Constants.USERS_PAGE,
+                        Constants.ADMIN_PAGE_PROD,
+                        Constants.USERS_PAGE_PROD
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
