2e81a37e0ff1f67e3d6dcb270f55746063b3196a
clothingquality <clothingquality69@gmail.com>
2025-07-25 16:30:18 -0500

    Cambio en origins

--- src/main/java/com/co/quality/clothing/config/SecurityConfig.java
+++ src/main/java/com/co/quality/clothing/config/SecurityConfig.java
@@ -36,16 +36,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*www.qualityclothingcol.com*",
-                "http://127.0.0.1:*", "*www.testsitedomainparapagina.io*"));
-        /*config.setAllowedOrigins(List.of(
+        config.setAllowedOrigins(List.of(
                 "https://www.qualityclothingcol.com",
                 "https://www.testsitedomainparapagina.io",
                 "https://qualityclothingcol.com",
                 "https://testsitedomainparapagina.io",
                 "http://127.0.0.1:5500",
                 "http://127.0.0.1:5502"
-        ));*/
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
